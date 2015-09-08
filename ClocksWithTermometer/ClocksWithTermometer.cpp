90ee29b67e2058f4c0e68635e34c25996c563a39
DTellurian <d.mihalych@gmail.com>
2015-09-08 12:11:22 +0300

    Creating first project

---
+++ ClocksWithTermometer/ClocksWithTermometer.cpp
@@ -0,0 +1,17 @@
+/*
+ * ClocksWithTermometer.cpp
+ *
+ * Created: 08.09.2015 12:10:57
+ *  Author: Mihalych
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
